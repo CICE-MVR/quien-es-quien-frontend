26ede7d06a3a24ac7abfc38d6fd8a6dda4327bdd
Maria Veronica Racca <veronica.racca@gmail.com>
2021-06-13 18:29:24 +0200

    Load logged in user from localstorage

--- src/features/login/hooks/useAuth.js
+++ src/features/login/hooks/useAuth.js
@@ -70,16 +70,13 @@ function useProvideAuth() {
   // ... component that utilizes this hook to re-render with the ...
   // ... latest auth object.
   useEffect(() => {
-    // const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-    //   if (user) {
-    //     setUser(user);
-    //   } else {
-    //     setUser(false);
-    //   }
-    // });
-    // // Cleanup subscription on unmount
-    // return () => unsubscribe();
-  }, []);
+    const userString = localStorage.getItem("user");
+    if (!user && userString) {
+      const loggedUser = JSON.parse(userString);
+      setUser(loggedUser);
+    }
+  }, [user]);
+
   // Return the user object and auth methods
   return {
     user,
--- src/features/login/ui/login/login.js
+++ src/features/login/ui/login/login.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { LoginForm } from "../../components/login-form/login-form";
 import { useAuth } from "../../hooks/useAuth";
 
 export const Login = () => {
-  const { signin } = useAuth();
+  const { signin, user } = useAuth();
   const [error, setError] = useState(false);
 
   const history = useHistory();
@@ -19,6 +19,12 @@ export const Login = () => {
     }
     setError(error);
   };
+
+  useEffect(() => {
+    if (user) {
+      history.replace(from);
+    }
+  }, [user, from, history]);
 
   return (
     <>
